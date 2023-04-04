7d75a79edf4c6415ad86c91b0336455ee6ee4ce6
Penta <pentacoloagustin@gmail.com>
2023-04-04 14:52:26 -0300

    Se agregaron las funciones ya existentes en el .h y se empezaron a implementar las funciones para el manejo de la cola circular en el .c - 04/04/2023

--- _drivers/Stream_I2C/inc/stream_i2c.h
+++ _drivers/Stream_I2C/inc/stream_i2c.h
@@ -5,12 +5,65 @@
 
   @author   Pentacolo Tomas
 
-  @date 	03/04/2023
+  @date 	  03/04/2023
 
   @version  v0.0.1 - Initial release.
  ******************************************************************************/
 
-//Variables recepcion
+//Definiciones
+
+#define is_Buffer_in_full(A)    (A&1)
+#define is_Buffer_out_full(A)   (A&2)
+#define is_Buffer_in_empty(A)   (A&4)
+#define is_Buffer_out_empty(A)  (A&8)
+
+#define Buffer_in_is_full(A)    (A|1)
+#define Buffer_out_is_full(A)   (A|2)
+#define Buffer_in_is_empty(A)   (A|4)
+#define Buffer_out_is_empty(A)  (A|8)
+
+#define is_Tramas_in_full(A)    (A&16)
+#define is_Tramas_in_empty(A)   (A&32)
+#define is_Tramas_out_full(A)   (A&64)
+#define is_Tramas_out_empty(A)  (A&128)
+
+#define Tramas_in_is_full(A)    (A|16)
+#define Tramas_in_is_empty(A)   (A|32)
+#define Tramas_out_is_full(A)   (A|64)
+#define Tramas_out_is_empty(A)  (A|128)
+
+#define I2CRFI_TOUT 300ul               //en decimas de segundo
+
+enum{
+	I2C_ESTADO_IDLE,
+	I2C_ESTADO_SENDING,
+	I2C_ESTADO_END
+};
+
+enum{
+	I2C_ESTADO_RCV_IDLE,
+	I2C_ESTADO_PKT_ID,
+	I2C_ESTADO_RCV_ID,
+	I2C_ESTADO_RCV_DATA
+};
+
+enum
+{
+	tipoTrama_noUsar=0,
+	tipoTrama_mensaje=1,
+	tipoTrama_ejecuable=2,
+	tipoTrama_control=3,
+	tipoTrama_bootloader=4,
+	tipoTrama_buffer=5
+};
+
+#ifdef _I2C_ID
+char I2C_ID = _I2C_ID;                  //ID del dispositivo.
+#else
+char I2C_ID = 0x64;                     //ID del dispositivo.
+#endif
+
+//Variables recepción
 
 uint8_t     Buffer_in[512];             //Buffer que contiene los bytes que se reciben por el i2c.
 uint16_t    Indice_lectura_in;          //Contiene la posicion del byte a leer.
@@ -20,7 +73,7 @@ uint16_t    Tramas_in[32];              //Contiene los contadores que indican cu
 uint8_t     Indice_lectura_trama_in;    //Indica la trama que se esta leyendo.
 uint8_t     Indice_escritura_trama_in;  //Indica la trama que se esta escribiendo.
 
-//Funciones recepcion
+//Funciones recepción
 
 void        push_in(char value);        //Agrega un nuevo byte a la trama asignada para escritura.
 char        pop_in(void);               //Obtiene un byte de la trama asignada para lectura.
@@ -28,6 +81,78 @@ char        pop_in(void);               //Obtiene un byte de la trama asignada p
 uint16_t    bytes_to_read(void);        //Obtiene la cantidad de bytes que le quedan a la trama de lectura actual.
 uint8_t     tramas_to_read(void);       //Obtiene la cantidad de tramas que se pueden leer.
 
-void        nueva_trama();              //Se ejecuta cuando comienza una nueva comunicación i2c.
-void        terminar_trama();           //Se ejecuta cuando finaliza la comunicación i2c.
+void        terminar_trama_in();        //Se ejecuta cuando finaliza la recepción del i2c.
 
+//Variables transmisión
+
+uint8_t     Buffer_out[256];            //Buffer que contiene los bytes que se reciben por el i2c.
+uint8_t     Indice_lectura_out;         //Contiene la posicion del byte a leer.
+uint8_t     Indice_escritura_out;       //Contiene la posicion del byte a escribir.
+
+uint8_t     Tramas_out[16];             //Contiene los contadores que indican cuantos bytes posee cada trama.
+uint8_t     Indice_lectura_trama_out;   //Indica la trama que se esta leyendo.
+uint8_t     Indice_escritura_trama_out; //Indica la trama que se esta escribiendo.
+
+uint8_t     last_package_length;        //Contiene el tamaño inicial de la ultima trama transmitida.
+
+//Funciones transmisión
+
+void        push_out(char value);       //Agrega un nuevo byte a la trama asignada para escritura.
+char        pop_out(void);              //Obtiene un byte de la trama asignada para la lectura.
+
+uint8_t     bytes_to_write(void);       //Obtiene la cantidad de bytes que le quedan a la trama de lectura actual.
+uint8_t     tramas_to_write(void);      //Obtiene la cantidad de tramas que se pueden transmitir.
+
+void        terminar_trama_out();       //Se ejecuta cuando finaliza la transmisión del i2c.
+
+//Variables de la maquina de estado y de configuración general
+
+char i2crfi_estado = I2C_ESTADO_IDLE;         //Estado en que se encuentra la maquina de estados.
+char i2crfi_estado_rcv = I2C_ESTADO_RCV_IDLE; //
+
+i2c_config_t i2crfi_config;                   //Estructura de configuración del i2c.
+
+uint32_t i2crfi_tout = 0;                     //Variable utilizada para el calculo del timeout.
+uint16_t i2crfi_tic = 0;                      //Variable utilizada para el calculo del timeout.
+
+uint8_t stream_i2c_flags = 0;
+/*
+ * bit 0: Buffer de recepción full.
+ * bit 1: Buffer de transmisión full.
+ * bit 2: Buffer de recepción vacio.
+ * bit 3: Buffer de transmisión vacio.
+ * bit 4: Buffer de tramas de recepción full.
+ * bit 5: Buffer de tramas de recepción vacio.
+ * bit 6: Buffer de tramas de transmisión full.
+ * bit 7: Buffer de tramas de transmisión vacio.
+ */
+
+//Streams
+
+const streamIn_t  getI2C = {Buffer_in, pop_in, bytes_to_read};
+const streamOut_t setI2C = {Buffer_out, push_out};
+
+//Interrupciones del i2c
+
+#ifdef S_I2C2
+void ISR_I2C2_SLAVE_CALLBACK(void);     //Se ejecuta cuando se recibe un byte por i2c.
+#else
+void ISR_I2C1_SLAVE_CALLBACK(void);     //Se ejecuta cuando se recibe un byte por i2c.
+#endif
+#ifdef S_I2C2
+void ISR_I2C2_MASTER_CALLBACK(void);     //Se ejecuta cuando se transmite un byte por i2c.
+#else
+void ISR_I2C1_MASTER_CALLBACK(void);     //Se ejecuta cuando se transmite un byte por i2c.
+#endif         
+
+//Funciones de inicialización y polling
+
+void stream_i2c_init(void);
+#setFile temp/EMICinits.c
+  stream_i2c_init();
+#unSetFile
+
+void stream_i2c_poll(void);
+#setFile temp/EMICpolling.c
+  stream_i2c_poll();
+#unSetFile
--- _drivers/Stream_I2C/src/stream_i2c.c
+++ _drivers/Stream_I2C/src/stream_i2c.c
@@ -10,3 +10,141 @@
   @version  v0.0.1 - Initial release.
  ******************************************************************************/
 
+void stream_i2c_init()
+{
+  AD1PCFG = 0xFFFF;
+	i2crfi_config.address = 'M';
+	i2crfi_config.frec = 400;
+#ifdef S_I2C2
+	i2crfi_config.i2c_chanel = 2;
+#else
+	i2crfi_config.i2c_chanel = 1;
+#endif
+	i2crfi_config.mode = I2C_MASTER_MODE;
+	i2crfi_config.en_interrupt = 1;
+	Init_I2C(i2crfi_config);
+
+  Indice_lectura_in = 0;
+  Indice_escritura_in = 0;
+  Indice_lectura_trama_in = 0;
+  Indice_escritura_trama_in = 0;
+
+  Indice_lectura_out = 0;
+  Indice_escritura_out = 0;
+  Indice_lectura_trama_out = 0;
+  Indice_escritura_trama_out = 0;
+
+  last_package_length = 0;
+
+  stream_i2c_flags = 0;
+}
+
+//Funciones de recepción
+
+
+void push_in(char value)
+{
+  if (!is_Buffer_in_full(stream_i2c_flags))                   //Si el buffer no esta lleno.
+  {
+    Buffer_in[Indice_escritura_in] = value;                   //Agrego el nuevo valor.
+    Tramas_in[Indice_escritura_trama_in]++;                   //Incremento la cantidad de bytes de la trama actual.
+
+    Indice_escritura_in++;                                    //Incremento el indice de escritura.
+    stream_i2c_flags = stream_i2c_flags & 0b11111011;         //El buffer deja de estar vacio.
+
+    if(Indice_escritura_in == Indice_lectura_in)              //Si el indice de escritura alcanzo al de lectura.
+    {
+      stream_i2c_flags = Buffer_in_is_full(stream_i2c_flags); //Se lleno el buffer asi que guardo esta condición.
+    }
+  }
+}
+
+char pop_in()
+{
+  char ret;
+  if (!is_Buffer_in_empty(stream_i2c_flags))                          //Si el buffer no esta vacio.
+  {
+    ret = Buffer_in[Indice_lectura_in];                               //Quito el byte correspondiente.
+    Tramas_in[Indice_lectura_trama_in]--;                             //Disminuyo la cantidad de bytes de la trama actual.
+
+    Indice_lectura_in++;                                              //Incremento el indice de lectura.
+    stream_i2c_flags = stream_i2c_flags & 0b11111110;                 //El buffer deja de estar lleno.
+
+    if(Indice_lectura_in == Indice_escritura_in)                      //Si el indice de lectura alcanzo al de escritura.
+    {
+      stream_i2c_flags = Buffer_in_is_empty(stream_i2c_flags);        //Significa que se vacio el buffer, por lo que guardo esta condición.
+    }
+
+    if (Tramas_in[indice_lectura_trama_in] == 0)                      //Si la trama se ha quedado sin bytes.
+    {
+      terminar_trama_lectura_in();                                    //Paso a la siguiente trama.
+    }
+  }
+}
+
+uint16_t bytes_to_read()
+{
+  uint16_t c2_IE = 65536 - Indice_escritura_in;     //Calculo el complemento a 2 del indice de escritura, el cual funciona de extremo.
+  return (Indice_lectura_in + c2_IE) & 0x03FF;      //Hago la diferencia entre el indice de lectura y el de escritura.
+}
+
+uint8_t tramas_to_read()
+{
+  uint8_t c2_IE = 256 - Indice_escritura_trama_in;  //Calculo el complemento a 2 del indice de escritura, el cual funciona de extremo.
+  return (Indice_lectura_trama_in + c2_IE) & 0x1F;  //Hago la diferencia entre el indice de lectura y el de escritura.
+}
+
+void terminar_trama_lectura_in()
+{
+  if (!is_Tramas_in_empty(stream_i2c_flags))                  //Si la trama no esta vacia.
+  {
+    Indice_lectura_trama_in++;                                //Salto a la siguiente trama a leer.
+    stream_i2c_flags = stream_i2c_flags & 0b11101111;         //El buffer de tramas deja de estar lleno.
+
+    if (Indice_lectura_trama_in == Indice_escritura_trama_in) //Si el indice de lectura alcanza al indice de escritura.
+    {
+      stream_i2c_flags = Trama_in_is_empty(stream_i2c_flags); //Significa que ya no hay más tramas disponibles.
+    }
+  }
+}
+
+void terminar_trama_escritura_in()
+{
+  if (!is_Tramas_in_full(stream_i2c_flags))                     //Si el buffer de tramas no esta lleno.
+  {
+    Indice_escritura_tramas_in++;                               //Incremento la cantidad de tramas disponibles para leer.
+    stream_i2c_flags = stream_i2c_flags & 0b11011111;           //El buffer no esta vacio.
+
+    if (Indice_escritura_tramas_in == Indice_lectura_tramas_in) //Si el indice de escritura alcanza al de lectura.
+    {
+      stream_i2c_flags = Trama_in_is_full(stream_i2c_flags);    //Significa que el buffer de tramas esta lleno.
+    }
+  }
+}
+
+//Funciones de transmisión
+
+void push_out(char value)
+{
+
+}
+
+char pop_out()
+{
+
+}
+
+uint16_t bytes_to_write()
+{
+
+}
+
+uint8_t tramas_to_write()
+{
+
+}
+
+void terminar_trama_out()
+{
+
+}
