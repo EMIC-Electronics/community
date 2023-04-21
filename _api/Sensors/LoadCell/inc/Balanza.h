374594c541c68c872f2eb4df69fa72f270f2b389
Penta <pentacoloagustin@gmail.com>
2023-04-21 13:46:43 -0300

    Falta implementar la deteccion de corrimiento y el poll de la balanza. Luego realizar los cambios del ADS1231 - 21/04/2023

--- _api/Sensors/LoadCell/inc/Balanza.h
+++ _api/Sensors/LoadCell/inc/Balanza.h
@@ -5,9 +5,9 @@
 
   @author   Pentacolo Tomas
 
-  @date     13/04/2023
+  @date     20/04/2023
 
-  @version  v0.0.1 - Initial release.
+  @version  v0.0.2 
  ******************************************************************************/
 
 //Variables accesibles para el usuario.
@@ -21,21 +21,36 @@ float mVxV_cal;
 doCMDfloat(mVxV_cal)    //mVxV value given in the specs of load cell.
 
 //Variables no accesibles para el usuario.
-float K;                //Slope of the linear function which describes the load cell.
-int32_t Cero;           //Intercept of the linear function which describes the load cell.
-int32_t Corrimiento;    //Offset due to the deformation of the load cell.
-int32_t Historial[32];  //Circular FIFO used to eliminate the noise of the load cell measure.
-int64_t Acumulador;     //Is the sumatory of all values contains in the FIFO.
+float K;                  //Slope of the linear function which describes the load cell.
+int32_t Cero;             //Intercept of the linear function which describes the load cell.
+float Varianza_cero;      //Is the simple variance of the measures in the cero point.
+int32_t Corrimiento;      //Offset due to the deformation of the load cell.
+int32_t Historial[32];    //Circular FIFO used to eliminate the noise of the load cell measure.
+int32_t ValorActual;      //Is the media of all values contains in the FIFO.
+float Varianza;           //Is the simple variance of the values contains in the FIFO.
+int64_t Acumulador;       //Is the sumatory of all values contains in the FIFO.
+
+int8_t Indice = 0;        //Position of the new value of the FIFO.
+
+int8_t flags = 0;         
+/*
+ * Bit 0: Stable. Indicates if the measure is stable.
+ * Bit 1: Zero. Indicates if the measure is zero.
+ * 
+ */
 
 //Funciones accesibles para el usuario
 
 void setZero(void);       //Sets the current value as zero.
-void setReference(float Peso_de_referencia);  //Defines the current value as the reference weight given as parameter.
+void setReference(float Peso_de_referencia);  //Defines the linear function that represent the load cell using the weight given as parameter.
+void setCapacity(float Capacity);       //Sets the value of Capacidad.
 void setmVxV(float mVxV); //Sets the value of mVxV_cal.
 
 //Funciones no accesibles para el usuario
 
-float nuevaLectura(int32_t nuevo_valor);  //Receives the new data from the sensor and applies the change in the weight value.
+void nuevaLectura(int32_t nuevo_valor); //Receives the new data from the sensor and applies the change in the weight value.
+void calcularVarianza(void);            //Calculates the variance of the data contains in the FIFO.
+void calcularCorrimiento(void);         //Determines the current values of Corrimiento.
 
 void init_Balanza(void);  //Initializes the correspondent variables for a correct work of the module.
 #setFile temp/EMICinits.c
@@ -49,9 +64,9 @@ void poll_Balanza(void);  //Iteration when the module logic is implemented.
 
 //Eventos
 
-extern void cero(void);
-extern void estable(Void);
-extern void capacidadMaximaSuperada(void);
+extern void cero(void);                     //Is executed when the weight value return to zero.
+extern void estable(Void);                  //Is executed when the weight value is stable after a variation.
+extern void capacidadMaximaSuperada(void);  //Is executed when the weight value is greater than the maximum capacity of the load cell.
 
 //----------------------------------------------------
 
--- _api/Sensors/LoadCell/src/Balanza.c
+++ _api/Sensors/LoadCell/src/Balanza.c
@@ -5,43 +5,113 @@
 
   @author   Pentacolo Tomas
 
-  @date     13/04/2023
+  @date     20/04/2023
 
-  @version  v0.0.1 - Initial release.
+  @version  v0.0.2
  ******************************************************************************/
-
-void sMax(int32_t Peso_conocido)
-{
-	Num = Peso_conocido;
-	Den = adConv - Ord;
-	return;
-}
-
-void sMin(void)
-{
-	Ord = adConv;
-	return;
-}
 
 void init_Balanza(void)
 {
 #ifdef PORT_ADS1231_Dout
 	init_ADS1231();
 #endif
+
+  K = 0;
+  Cero = 0;
+  Corrimiento = 0;
+
+  for (int i = 0; i < 32; i++)
+    Historial[i] = 0;
+
+  ValorActual = 0;
+  Varianza = 0;
+
+  Acumulador = 0;
 }
 
-void DynaOn(void)
+void setZero(void)
 {
-	DynaOn_();
+  if (flags & 0x01)                 //If the measure is stable.
+  {
+    Cero = ValorActual;
+    Varianza_cero = getVarianza();
+  }
 }
 
-void DynaOff(void)
+void setReference(float Peso_de_referencia)
 {
-	DynaOff_();
+  if (flags & 0x01)                 //If the measure is stable.
+  {
+    K = ReferenceValue/(float)(ValorActual - Cero);   //Determines the slope of the linear function that represent the load cell.
+  }
+}
+
+void setCapacity(float Capacity)
+{
+  Capacidad = Capacity;
+}
+
+void setmVxV(float mVxV)
+{
+  mVxV_cal = mVxV;
+}
+
+void nuevaLectura(int32_t nuevo_valor)
+{
+
+  if(((nuevo_valor - Corrimiento) - valorActual)&(0x00FFFFFF) >= Varianza * 5)      //If the dispertion of the value is grater.
+  {
+    Acumulador = 0;                                                 //Replace all values for the new value.
+    for (int i = 0; i < 31; i++)
+    {
+      Historial[i] = (nuevo_valor - Corrimiento);
+      Acumulador += (nuevo_valor - Corrimiento);
+    }
+
+    Indice = 0;
+
+    ValorActual = (nuevo_valor - Corrimiento);
+
+    flags &= 0b11111110;                    //Isn´t stable.
+  }
+  else
+  {
+    flags |= 0b00000001;                    //Is stable.
+    Acumulador -= Historial[Indice];
+    Acumulador += (nuevo_valor - Corrimiento);
+    Historial[Indice] = (nuevo_valor - Corrimiento);
+
+    Indice++;
+    Indice &= 0x1F;                         //Clamp the index between 0 and 32.
+  
+    ValorActual = Acumulador/32;            //Obtains the media of the values contains in the FIFO.
+  }
+
+  Varianza = getVarianza();                 //Refresh the variance info.
+
+  Peso = (ValorActual - Cero)(float) * K;   //Refresh the current weight value.
+}
+
+float calcularVarianza(void)
+{
+  int64_t accum = 0;
+  for (int i = 0; i < 32; i++)
+    accum += (Historial[i] - ValorActual)^2;
+  
+  return (float)accum/(float)31;
+}
+
+void calcularCorrimiento(void)
+{
+  
 }
 
 void Balanza_poll(void)
 {
+  
+
+
+
 	// if (ADS123xFlag_adc)...
     ADS123xFlag_adc = 0;
     balanzaActive = 0;
