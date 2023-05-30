3742ea1c97f12f166893e7b16caf3655ab0e5684
mhunkeler <mariano.hunkeler@rfindustrial.com>
2023-05-30 11:41:54 -0300

    Se cambio logica del frame identefier en  uart

---
+++ _drivers/USB/MCP2200_v1.1.0/src/mcp2200.c
@@ -0,0 +1,34 @@
+/*************************************************************************//**
+
+  @file     USB_MCP2200.c
+
+  @brief    Driver Library to use USB
+
+  @author   Tomas Pentacolo (based on Pablo Llull (PL))
+
+  @date     23/11/2022
+
+  @version  v0.0.1 - Initial release.
+  
+ ******************************************************************************/
+
+void Init_USBDriver()
+{
+	#ifdef PORT_MCP2200_RST
+	HAL_PinMap_MCP2200_RST(PinMap_DIGITAL);
+    HAL_PinMap_MCP2200_SSPND(PinMap_DIGITAL);
+    HAL_PinMap_MCP2200_USBCFG(PinMap_DIGITAL);
+    HAL_PinMap_MCP2200_RX(PinMap_DIGITAL);
+    HAL_GPIO_PinCfg(MCP2200_RX,GPIO_INPUT);
+    HAL_GPIO_PinCfg(MCP2200_TX,GPIO_OUTPUT);
+	TRIS_MCP2200_RST = 0;
+	PIN_MCP2200_RST = 1;
+	#endif
+	
+	UARTX_{puerto}__init();
+}
+
+
+void Poll_USBDriver(void)
+{
+}
